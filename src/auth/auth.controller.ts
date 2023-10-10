91a5e6a3014867666e6c92908de11f2c2ab757a9
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2023-10-10 16:16:33 -0300

    feat: implements jwt validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,13 +7,15 @@ import {
   Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
+import { IsPublic } from './decorators/is-public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @IsPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,21 +1,48 @@
+// NestJS
 import {
   ExecutionContext,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+// Password
 import { AuthGuard } from '@nestjs/passport';
+// Decorators
+
+// Error Handling
+import { UnauthorizedError } from '../errors/unauthorized.error';
+import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {
-  canActivate(context: ExecutionContext) {
-    return super.canActivate(context);
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
   }
 
-  handleRequest(err, user) {
-    if (err || !user) {
-      throw new UnauthorizedException(err?.message);
+  canActivate(context: ExecutionContext): Promise<boolean> | boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
     }
 
-    return user;
+    const canActivate = super.canActivate(context);
+
+    if (typeof canActivate === 'boolean') {
+      return canActivate;
+    }
+
+    const canActivatePromise = canActivate as Promise<boolean>;
+
+    return canActivatePromise.catch((error) => {
+      if (error instanceof UnauthorizedError) {
+        throw new UnauthorizedException(error.message);
+      }
+
+      throw new UnauthorizedException();
+    });
   }
 }
