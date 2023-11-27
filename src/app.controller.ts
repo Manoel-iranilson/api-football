f5fb8ce23951001621c4002a21b6f696cfd65073
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2023-11-27 16:09:55 -0300

    feat: get test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CurrentUser } from './auth/decorators/current-user.decorator';
 import { User } from './user/entities/user.entity';
+import { IsPublic } from './auth/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
@@ -10,5 +11,11 @@ export class AppController {
   @Get('me')
   getMe(@CurrentUser() user: User) {
     return user;
+  }
+
+  @IsPublic()
+  @Get('')
+  getTest() {
+    return 'hello word';
   }
 }
